a270b3a136bc79111fe9fe033c89b85894b69453
anshawasthi88 <109279554+anshawasthi88@users.noreply.github.com>
2024-02-11 15:07:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -104,7 +104,7 @@ function renderWeatherInfo(weatherInfo) {
     countryIcon.src = `https://flagcdn.com/144x108/${weatherInfo?.sys?.country.toLowerCase()}.png`;
     desc.innerText = weatherInfo?.weather?.[0]?.description;
     weatherIcon.src = `http://openweathermap.org/img/w/${weatherInfo?.weather?.[0]?.icon}.png`;
-    temp.innerText = `${weatherInfo?.main?.temp} °C `;
+    temp.innerText = `${Math.floor(weatherInfo?.main?.temp)} °C`;
     windspeed.innerText = `${weatherInfo?.wind?.speed} m/s`;
     humidity.innerText = `${weatherInfo?.main?.humidity}%`;
     cloudiness.innerText = `${weatherInfo?.clouds?.all}%`;
